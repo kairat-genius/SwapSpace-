a1f6138ed4511df000b49589a00f8c7e2720a8af
kairat <112894056+kairat-genius@users.noreply.github.com>
2024-02-07 14:12:10 +0300

    tesy

--- backend/project/models/tag/tag.py
+++ backend/project/models/tag/tag.py
@@ -1,9 +1,18 @@
 from settings.db import Base
-from sqlalchemy import Column, ForeignKey, String, Integer, Text, DateTime
+from sqlalchemy import Column, ForeignKey, String, Integer, Text, DateTime, Table
 from sqlalchemy.orm import relationship
 
+
+
+product_tag_association = Table('product_tag_association', Base.metadata,
+                                Column('product_id', Integer, ForeignKey('Product.id')),
+                                Column('tag_id', Integer, ForeignKey('Tag.id'))
+                                )
 class Tag(Base):
     __tablename__ = "Tag"
     id = Column(Integer, primary_key=True, index=True, unique=True)
     title = Column(String)
     urls = Column(String, unique=True)
+
+    # Определяем обратное отношение для продуктов через промежуточную таблицу
+    products = relationship("Product", secondary=product_tag_association, back_populates="tags")
